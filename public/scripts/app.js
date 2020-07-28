eb9ce510e15409ad836720659b62047d1ce5f290
Dane Mancuso <danegmancuso@gmail.com>
2020-07-28 21:18:20 +0000

    refactored ejs files to include a 'head' file that contains head info

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,16 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+
+  $('#search-property-form').submit((event) => {
+    event.preventDefault();
+
+    // post request to get search results returned.
+
+    // in the then of the ajax, append results list to the empty content container in index.ejs. (use template literals)
+
+    console.log('search form submitted')
+
+  });
+
 });
