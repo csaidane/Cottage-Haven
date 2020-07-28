370a5c0571579200bf7b350bc300ecd320e2c9e8
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-07-28 17:26:51 +0000

    Added messages.js and messages_index.ejs to get messaging routes started. Added a POST route to /messages that takes the user to the messages page when clicking on the 'messages' link in the nave bar.Started the GET route to allow user to send messages but this is not complete yet.

---
+++ routes/messages.js
@@ -0,0 +1,66 @@
+"use strict";
+
+//Boilerplate used from Tweeter project with modifications (removal of random user generation)
+
+const express       = require('express');
+const messagesRoutes  = express.Router();
+const {getMessages} = require('./helper_functions');
+
+//WHERE IS DATAHELPERS USED IN ANOTHER FILE??? WHAT IS A DATAHELPER - CALLBACK?? WHERE IS IT COMING FROM??
+module.exports = function() {
+
+  const getUserMessages =  function(userId) {
+    return getMessages(userId)
+    .then(message => {
+     return messages
+    });
+  }
+  exports.getUserMessages = getUserMessages;
+
+  //If user clicks on messages in nav, this redirects user to messages page where they can see messages they've received
+  messagesRoutes.get("/messages", function(req, res) {
+    const userId = cookie; //grab cookie here with req.cookie or req.session
+    return getUserMessages(userId)
+    .then((messages => {
+      console.log(userId)
+      if (messages.length === 0) {
+        res.send({error: "No messages to show"});
+        return;
+      }
+      res.render('messages_index', messages);
+
+    })
+    .catch(e => res.send(e))
+
+  });
+
+  //Allows user to send messages - body of function needs to be updated
+  messagesRoutes.post("/messages", function(req, res) {
+    //user name (user_id) selected from drop down in compose message form becomes sender_id
+
+    if (!req.body.text) {
+      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      return;
+    }
+
+    const user = req.body.user
+    const message = {
+      user: user,
+      content: {
+        text: req.body.text
+      },
+      created_at: Date.now()
+    };
+
+    DataHelpers.saveMessage(message, (err) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else {
+        res.status(201).send();
+      }
+    });
+  });
+
+  return messagesRoutes;
+
+}
