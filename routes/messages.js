93bda163c5de28690339c657f6016049bfc0417b
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-07-28 20:28:15 +0000

    Added a POST route and accompanying helper functions to enable users to send messages. Also created a placeholder sent_confirm.ejs file to display message sent confirmation.

--- routes/messages.js
+++ routes/messages.js
@@ -21,11 +21,12 @@ module.exports = function() {
   }
   exports.getUserMessages = getUserMessages;
 
-  //If user clicks on messages in nav, this redirects user to messages page where they can see messages they've received
+  //If user clicks on messages in nav, this redirects user to messages page where they can see messages they've received. It also pull all users into the compose message form in the "to" drop down menu to select a receiver.
   messagesRoutes.get("/messages", function(req, res) {
     const userId = req.session.user_id;
     return getUserMessages(userId)
     .then((messages => {
+      const allUsers = getAllUsers(userId); //this may need a .then(data) and return data
       console.log(userId)
       if (messages.length === 0) {
         res.send({error: "No messages to show"});
@@ -38,33 +39,25 @@ module.exports = function() {
 
   });
 
-  //Allows user to send messages - body of function needs to be updated
+  //Allows user to send messages to the database and redirects user to message sent confirmation page
   messagesRoutes.post("/messages", function(req, res) {
-    //user name (user_id) selected from drop down in compose message form becomes sender_id
+    //user name (user_id) selected from drop down in compose message form becomes receiver_id - front end
 
-    if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body'});
-      return;
-    }
-
-    const user = req.body.user
-    const message = {
-      user: user,
-      content: {
-        text: req.body.text
-      },
-      created_at: Date.now()
-    };
-
-    DataHelpers.saveMessage(message, (err) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.status(201).send();
+    const fullMessage = {sender_id : req.session.user_id, content : req.body.content, receiver_id : req.body.receiver_id}; //these .names have to be the same on the front end so that selected username can be converted to receiver_id <select name="receiver_id"><option>NAME</option><select> <--- add to front end
+    sendMessage(fullMessage)
+    .then(message => {
+      if (!message) {
+        res.send({error: "No message sent"});
+        return;
       }
-    });
+      res.render('sent_confirm');
+    })
+    .catch(e => res.send(e));
+
   });
 
   return messagesRoutes;
+
+
 
 }
